6b0251bc25e7105a4ec270c15edebc6b8be79dcb
rej <rzioma@acm.org>
2024-04-21 17:15:27 +0200

    Should be able to fit in 4 tiles

--- src/config.tcl
+++ src/config.tcl
@@ -12,7 +12,7 @@
 
 # PL_TARGET_DENSITY - You can increase this if Global Placement fails with error GPL-0302.
 # Users have reported that values up to 0.8 worked well for them.
-set ::env(PL_TARGET_DENSITY) 0.6
+set ::env(PL_TARGET_DENSITY) 0.7
 
 # CLOCK_PERIOD - Increase this in case you are getting setup time violations.
 # The value is in nanoseconds, so 20ns == 50MHz.
