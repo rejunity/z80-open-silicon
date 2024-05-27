40c829aa420143a568f08325d218e0ea03222ff1
rej <rzioma@acm.org>
2024-05-27 21:21:06 +0200

    mapped all pins

--- src/ci2406_z80.v
+++ src/ci2406_z80.v
@@ -26,9 +26,12 @@ module ci2406_z80(
     wire z80_clk =  wb_clk_i;
     wire ena =      1'b1;
 
-    // I took ChipIgnite ASIC pinout as a reference from https://github.com/efabless/clear
+    // I took ChipIgnite "ASIC pinout" as a reference from the https://github.com/efabless/clear/tree/RTL-dev
     // Hope to map to Z80 pins with minimal wire crossing.
-    
+
+    // Also to double-check the ZX spectrum schematics might be useful:
+    // https://spectrumforeveryone.com/wp-content/uploads/2017/08/ZXSpectrumIssue2-Schematics.gif
+
     // Assigning pins in Counter Clockwise order:
     //   Z80) starting roughly from a bottom left corner, pin 18 (/HALT).
     //   CI) starting from a bottom right corner, pin 31 (mprj_io[0]).
@@ -38,21 +41,21 @@ module ci2406_z80(
 
     //                                 Z80 CPU
     //                 ,----------------.___.----------------.
-    //      <--    A11 |1  -    .                    .   - 40| A10    -->
-    //      <--    A12 |2  -    .                    .   - 39| A9     -->
-    //      <--    A13 |3  -    .                    .   - 38| A8     -->
-    //      <--    A14 |4  - io[22] 60               .   - 37| A7     -->
-    //      <--    A15 |5  - io[23] 61               .   - 36| A6     -->
-    //      -->    CLK |6  -  xclk  22?              .   - 35| A5     -->
-    //      <->     D4 |7  - io[24] 62               .   - 34| A4     -->
-    //      <->     D3 |8  - io[25]  2               .   - 33| A3     -->
-    //      <->     D5 |9  -    .                    .   - 32| A2     -->
-    //      <->     D6 |10 -    .              43 io[9]  - 31| A1     -->
-    //         VCC_5V0 |11 -                   42 io[8]  - 30| A0     -->
-    //      <->     D2 |12 -    .                          29| GND
-    //      <->     D7 |13 -    .              41 io[7]  - 28| /RFSH  --> 
-    //      <->     D0 |14 -    .              33 io[2]  - 27| /M1    -->
-    //      <->     D1 |15 - io[31]  8        ?21  rst   - 26| /RESET <--
+    //      <--    A11 |1  - io[19] 57         55 io[18] - 40| A10    -->
+    //      <--    A12 |2  - io[20] 58         54 io[17] - 39| A9     -->
+    //      <--    A13 |3  - io[21] 59         53 io[16] - 38| A8     -->
+    //      <--    A14 |4  - io[22] 60         51 io[15] - 37| A7     -->
+    //      <--    A15 |5  - io[23] 61         50 io[14] - 36| A6     -->
+    //      -->    CLK |6  -  xclk  22(?)      48 io[13] - 35| A5     -->
+    //      <->     D4 |7  - io[24] 62         46 io[12] - 34| A4     -->
+    //      <->     D3 |8  - io[25]  2         45 io[11] - 33| A3     -->
+    //      <->     D5 |9  - io[26]  3         44 io[10] - 32| A2     -->
+    //      <->     D6 |10 - io[27]  4         43 io[9]  - 31| A1     -->
+    //         VCC_5V0 |11                     42 io[8]  - 30| A0     -->
+    //      <->     D2 |12 - io[28]  5                     29| GND
+    //      <->     D7 |13 - io[29]  6         41 io[7]  - 28| /RFSH  --> 
+    //      <->     D0 |14 - io[30]  7         33 io[2]  - 27| /M1    -->
+    //      <->     D1 |15 - io[31]  8      (?)21  rst   - 26| /RESET <--
     //      -->   /INT |16 - io[32] 11         14 io[35] - 25| /BUSRQ <--
     //      -->   /NMI |17 - io[33] 12         13 io[34] - 24| /WAIT  <--
     //      <--  /HALT |18 - io[0]  31         32 io[1]  - 23| /BUSAK -->
