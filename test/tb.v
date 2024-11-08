20368bb4ef3f68c695ba608854c9e61815e0709c
rej <rzioma@acm.org>
2024-11-08 22:18:50 +0100

    Updated VPWR/VGND ports inside GL_TEST to match new template tb.v

--- test/tb.v
+++ test/tb.v
@@ -13,24 +13,28 @@ module tb ();
     #1;
   end
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+    // wire up the inputs and outputs
+    wire [7:0] ui_in;
+    wire [7:0] uo_out;
+    wire [7:0] uio_in;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
+    wire clk;
+    wire rst_n;
+    wire ena;
+`ifdef GL_TEST
+    wire VPWR = 1'b1;
+    wire VGND = 1'b0;
+`endif
 
   // Replace tt_um_example with your module name:
   tt_um_rejunity_z80 user_project (
 
       // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
-`endif
+      `ifdef GL_TEST
+          .VPWR(VPWR),
+          .VGND(VGND),
+      `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
