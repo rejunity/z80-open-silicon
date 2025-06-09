462995f2fb40ed47f39ae0679469dbf6ffd65125
rej <rzioma@acm.org>
2025-06-09 11:36:25 +0300

    TT10 clear comment about ena always 1

--- src/tt_um_rejunity_z80.v
+++ src/tt_um_rejunity_z80.v
@@ -11,7 +11,7 @@ module tt_um_rejunity_z80 (
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
